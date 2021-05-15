a1932ea204427fc50ed24a6568d60f3863785d32
marijndegen <marijndegen96@hotmail.com>
2021-05-15 13:23:04 +0200

    initial project scaffold

---
+++ code/wapp-demo/wapp-demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wapp_demo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
